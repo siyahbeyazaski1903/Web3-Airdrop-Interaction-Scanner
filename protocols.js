13851d566bdd82c83cd83e08b91eb43bf5fbe6e6
siyahbeyazaski1903 <siyahbeyazaski1903@gmail.com>
2026-03-10 16:37:49 +0700

    Create protocols.js

---
+++ protocols.js
@@ -0,0 +1,18 @@
+const PROTOCOLS = [
+
+{
+name:"Uniswap",
+address:"0xE592427A0AEce92De3Edee1F18E0157C05861564"
+},
+
+{
+name:"Zora",
+address:"0xABCD000000000000000000000000000000000000"
+},
+
+{
+name:"LayerZero",
+address:"0x66A71Dcef29A0fFBDBE3c6a460a3B5BC225Cd675"
+}
+
+]
